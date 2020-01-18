28cf66c48ade6aa6b02cbc91bbd4719ce61cc64c
yymin1022 <yymin1022@gmail.com>
2020-01-18 22:30:14 +0900

    Update Overlay Structure

--- app/src/main/java/com/yong/notchhider/HideService.java
+++ app/src/main/java/com/yong/notchhider/HideService.java
@@ -14,6 +14,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 
 import androidx.core.app.NotificationCompat;
@@ -22,12 +23,48 @@ public class HideService extends Service {
     LayoutInflater inflater;
     NotificationCompat.Builder notificationBuilder;
     NotificationManager notificationManager;
-    View windowView;
-    WindowManager windowManager;
-    WindowManager.LayoutParams windowParams;
+    View windowViewOverscan;
+    View windowViewStatusbar;
+    WindowManager windowManagerOverscan;
+    WindowManager windowManagerStatusbar;
+    WindowManager.LayoutParams windowParamsOverscan;
+    WindowManager.LayoutParams windowParamsStatusbar;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        /* Overscan Area Overlay */
+        windowManagerOverscan = (WindowManager)getSystemService(WINDOW_SERVICE);
+        windowParamsOverscan = new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                /*ViewGroup.LayoutParams.WRAP_CONTENT*/ 90,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN|
+                WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR|
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE|
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.RGB_888
+        );
+        windowParamsOverscan.gravity = Gravity.TOP;
+        windowParamsOverscan.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        windowViewOverscan = inflater.inflate(R.layout.window_overscan, null);
+        windowManagerOverscan.addView(windowViewOverscan, windowParamsOverscan);
+
+        /* Statusbar Area Overlay */
+        windowManagerStatusbar = (WindowManager)getSystemService(WINDOW_SERVICE);
+        windowParamsStatusbar = new WindowManager.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                /*ViewGroup.LayoutParams.WRAP_CONTENT*/ 90,
+                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN|
+                WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE|
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.RGB_888
+        );
+        windowParamsStatusbar.gravity = Gravity.TOP;
+        windowViewStatusbar = inflater.inflate(R.layout.window_statusbar, null);
+        windowManagerStatusbar.addView(windowViewStatusbar, windowParamsStatusbar);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
@@ -50,28 +87,14 @@ public class HideService extends Service {
         if(notificationBuilder != null && notificationManager != null){
             startForeground(1379, notificationBuilder.build());
         }
-
-        inflater = (LayoutInflater)getSystemService(LAYOUT_INFLATER_SERVICE);
-
-        windowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
-        windowParams = new WindowManager.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.RGB_888
-        );
-        windowParams.gravity = Gravity.TOP;
-        windowView = inflater.inflate(R.layout.window_view, null);
-
-        windowManager.addView(windowView, windowParams);
-
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        windowManagerOverscan.removeView(windowViewOverscan);
+
         stopForeground(true);
     }
 
