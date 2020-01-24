b8a6611410e3e02ce5a041eef267638e5635ae0a
yymin1022 <yymin1022@gmail.com>
2020-01-25 00:28:46 +0900

    Update MainActivity ImageView Resource

--- app/src/main/java/com/yong/notchhider/MainActivity.java
+++ app/src/main/java/com/yong/notchhider/MainActivity.java
@@ -2,42 +2,69 @@ package com.yong.notchhider;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    Boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnStart = findViewById(R.id.main_btn_start);
-        Button btnStop = findViewById(R.id.main_btn_stop);
 
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch(view.getId()){
-                    case R.id.main_btn_start:
-                        startService(new Intent(MainActivity.this, HideService.class));
-                        break;
-                    case R.id.main_btn_stop:
-                        stopService(new Intent(MainActivity.this, HideService.class));
-                        break;
-                }
-            }
-        };
-        btnStart.setOnClickListener(onClickListener);
-        btnStop.setOnClickListener(onClickListener);
-
+        LinearLayout mainLayout = findViewById(R.id.main_layout);
         SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+
         if(prefs.getBoolean("isFirst", true)){
             startActivity(new Intent(this, PermissionActivity.class));
+        }
+
+        updateView();
+
+        mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isRunning){
+                    stopService(new Intent(getApplicationContext(), HideService.class));
+                }else{
+                    startService(new Intent(getApplicationContext(), HideService.class));
+                }
+                updateView();
+            }
+        });
+
+    }
+
+    void updateView(){
+        ActivityManager manager = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+        ImageView ivNotch = findViewById(R.id.main_image_notch);
+        TextView tvInfo = findViewById(R.id.main_text_info);
+
+        ivNotch.setImageResource(R.drawable.ic_service_off);
+        tvInfo.setText("OFF");
+        isRunning = false;
+
+        if(manager != null){
+            for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+                if (HideService.class.getName().equals(service.service.getClassName())) {
+                    ivNotch.setImageResource(R.drawable.ic_service_on);
+                    tvInfo.setText("ON");
+                    isRunning = true;
+                }
+            }
         }
     }
 }
